053220bf221f9560f8c86e0af302a47da5ebfc75
ae95caba <ae95caba@gmail.com>
2023-10-17 03:07:15 -0300

    fix tabs not showing in aside

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,52 +5,38 @@ import logo from "../assets/react.svg";
 import LogoutButton from "./LogoutButton";
 import UserContext from "../Contexts/UserContext";
 
-const tabsArr = ["a", "b", "c"];
-
 export default function Header() {
   const { isUserOnline, setIsUserOnline } = useContext(UserContext);
+
+  const tabs = (
+    <ul>
+      {isUserOnline ? (
+        <li>
+          <LogoutButton setIsUserOnline={setIsUserOnline} />
+        </li>
+      ) : (
+        <>
+          <li>
+            <Signup />
+          </li>
+          <li>
+            <Signin setIsUserOnline={setIsUserOnline} />
+          </li>
+        </>
+      )}
+    </ul>
+  );
   return (
     <section id="header">
       <a className="logo" href="">
         <img src={logo} alt="" />
       </a>
-      <nav>
-        <ul>
-          {isUserOnline ? (
-            <li>
-              <LogoutButton setIsUserOnline={setIsUserOnline} />
-            </li>
-          ) : (
-            <>
-              <li>
-                <Signup />
-              </li>
-              <li>
-                <Signin setIsUserOnline={setIsUserOnline} />
-              </li>
-            </>
-          )}
-        </ul>
-      </nav>
+      <nav>{tabs}</nav>
 
       <input type="checkbox" id="checkbox" />
 
-      <Sidebar />
+      <aside>{tabs}</aside>
       <label className="burger-menu" htmlFor="checkbox"></label>
     </section>
-  );
-}
-
-function Sidebar() {
-  return (
-    <aside>
-      <ul>
-        {tabsArr.map((tab) => (
-          <li>
-            <a href="#">{tab}</a>
-          </li>
-        ))}
-      </ul>
-    </aside>
   );
 }
