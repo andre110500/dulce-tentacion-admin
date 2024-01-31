196e9e605bb2e818c998246059fbdbfad0a8c43a
ae95caba <ae95caba@gmail.com>
2024-01-31 04:02:22 -0300

    add parameter to build hook to clear cache

--- src/functions/runBuild.tsx
+++ src/functions/runBuild.tsx
@@ -1,6 +1,6 @@
 export default async function runBuild() {
   const buildHookUrl =
-    "https://api.netlify.com/build_hooks/65b9b29b225667753fab3a34";
+    "https://api.netlify.com/build_hooks/65b9b29b225667753fab3a34?clear_cache=true";
 
   try {
     const response = await fetch(buildHookUrl, {
