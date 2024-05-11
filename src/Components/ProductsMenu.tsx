d6462e5f469804d06ead078b959e0a1149167d01
ae95caba <ae95caba@gmail.com>
2024-05-10 21:19:57 -0300

    handle the posibility of not being products and hidden outOfStock products from the menu

--- src/Components/ProductsMenu.tsx
+++ src/Components/ProductsMenu.tsx
@@ -5,7 +5,7 @@ export default function ProductsMenu({ productsList }) {
   const sortedProductsList = productsList.sort((a, b) => b.price - a.price);
 
   function getPriceByFlavoursQuantity(number) {
-    return sortedProductsList.find((obj) => obj.flavours === number).price;
+    return sortedProductsList.find((obj) => obj.flavours === number)?.price;
   }
 
   return (
@@ -14,11 +14,14 @@ export default function ProductsMenu({ productsList }) {
 
       <div className="uls-container">
         <ul className="first">
-          {sortedProductsList.map((product) => (
-            <li key={product._id}>
-              <span>{product.name}</span> <span>${product.price}</span>
-            </li>
-          ))}
+          {sortedProductsList.map(
+            (product) =>
+              !product.outOfStock && (
+                <li key={product._id}>
+                  <span>{product.name}</span> <span>${product.price}</span>
+                </li>
+              )
+          )}
         </ul>
         <h2>Combos con descuento:</h2>
         <ul>
@@ -44,9 +47,12 @@ export default function ProductsMenu({ productsList }) {
             <span>2u 1/4 kg</span>
             <p>
               <span className="line-through">
-                ${getPriceByFlavoursQuantity(2) * 2}
+                ${getPriceByFlavoursQuantity(2) * 2 || "undefined"}
               </span>
-              <span> ${getPriceByFlavoursQuantity(2) * 2 - 200}</span>
+              <span>
+                {" "}
+                ${getPriceByFlavoursQuantity(2) * 2 - 200 || "undefined"}
+              </span>
             </p>
           </li>
         </ul>
