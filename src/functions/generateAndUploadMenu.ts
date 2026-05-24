50266bcb1de3e196b63ce35b6495b813660eb178
André Espinoza <ae95caba@gmail.com>
2026-05-24 19:49:24 -0300

    fix: reduce canvas scale for menu generation

--- src/functions/generateAndUploadMenu.ts
+++ src/functions/generateAndUploadMenu.ts
@@ -9,7 +9,7 @@ export async function generateAndUploadMenu(menuId: string) {
         return;
     }
 
-    const canvas = await html2canvas(element, { scale: 4, useCORS: true });
+    const canvas = await html2canvas(element, { scale: 1, useCORS: true });
 
     return new Promise<void>((resolve, reject) => {
         canvas.toBlob(async (blob) => {
