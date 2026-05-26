e370bf533f61f186e3a591c159ffb9a046f6969f
André Espinoza <ae95caba@gmail.com>
2026-05-25 22:22:25 -0300

    feat: automate build info generation with post-commit hooks and pre-dev scripts

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,9 +1,9 @@
 export const buildInfo = {
-  "commitHash": "9593bdac03f864af86b4edebdb3c8252870e4d95",
-  "shortCommitHash": "9593bda",
-  "commitTimestamp": "2026-05-25T16:06:37-03:00",
+  "commitHash": "a13cfcdb459d87f992da33e601cbe6065fb53eb5",
+  "shortCommitHash": "a13cfcd",
+  "commitTimestamp": "2026-05-25T22:09:47-03:00",
   "branch": "main",
   "deployId": "local",
-  "buildTimestamp": "2026-05-25T19:27:01.989Z",
+  "buildTimestamp": "2026-05-26T01:14:27.176Z",
   "githubRepo": "ae95caba/dulce-tentacion-admin"
 } as const;
