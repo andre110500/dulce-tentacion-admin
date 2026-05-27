c5688964cc51424d7eff5a5a2e5a1ee4a1102b2c
André Espinoza <ae95caba@gmail.com>
2026-05-27 19:43:06 -0300

    style: reduce add-ons font size

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,9 +1,9 @@
 export const buildInfo = {
-  "commitHash": "7a2dd008942ad22ba356b94fad26a4111222069a",
-  "shortCommitHash": "7a2dd00",
-  "commitTimestamp": "2026-05-27T19:22:44-03:00",
+  "commitHash": "df68a183d15a796eb7e8a247a992f62d55ea0e42",
+  "shortCommitHash": "df68a18",
+  "commitTimestamp": "2026-05-27T19:26:45-03:00",
   "branch": "main",
   "deployId": "local",
-  "buildTimestamp": "2026-05-27T22:22:45.885Z",
+  "buildTimestamp": "2026-05-27T22:26:46.301Z",
   "githubRepo": "ae95caba/dulce-tentacion-admin"
 } as const;
