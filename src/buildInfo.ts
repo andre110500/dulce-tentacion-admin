fce1cba51b64b405aba1407f4e66ea28b9e21d69
André Espinoza <ae95caba@gmail.com>
2026-06-03 23:51:39 -0300

    chore: update menu images with new logo

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,9 +1,9 @@
 export const buildInfo = {
-  "commitHash": "df68a183d15a796eb7e8a247a992f62d55ea0e42",
-  "shortCommitHash": "df68a18",
-  "commitTimestamp": "2026-05-27T19:26:45-03:00",
+  "commitHash": "687925a343d0c24a359a721d6eddec8be990d967",
+  "shortCommitHash": "687925a",
+  "commitTimestamp": "2026-05-27T19:43:06-03:00",
   "branch": "main",
   "deployId": "local",
-  "buildTimestamp": "2026-05-27T22:26:46.301Z",
+  "buildTimestamp": "2026-05-29T08:13:28.053Z",
   "githubRepo": "ae95caba/dulce-tentacion-admin"
 } as const;
