448db658640613a36c16689fae6965150316a361
André Espinoza <ae95caba@gmail.com>
2026-06-08 00:43:57 -0300

    feat: update buildInfo with new commit details and enhance product card styles in index.scss

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,9 +1,9 @@
 export const buildInfo = {
-  "commitHash": "816ff117c554698beb4d6c0bb4e148f1f75967eb",
-  "shortCommitHash": "816ff11",
-  "commitTimestamp": "2026-06-07T23:20:26-03:00",
+  "commitHash": "381369a95b5797961cd1293bb8eea5121d7f9b7f",
+  "shortCommitHash": "381369a",
+  "commitTimestamp": "2026-06-07T23:55:51-03:00",
   "branch": "main",
   "deployId": "local",
-  "buildTimestamp": "2026-06-08T02:20:26.431Z",
+  "buildTimestamp": "2026-06-08T02:55:52.223Z",
   "githubRepo": "ae95caba/dulce-tentacion-admin"
 } as const;
