e52c7ad1c61770a779c14fd2cd2cc22ae4c95ff3
André Espinoza <ae95caba@gmail.com>
2026-06-10 03:52:09 -0300

    feat: adjust styles in index.scss for improved layout and responsiveness

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,9 +1,9 @@
 export const buildInfo = {
-  "commitHash": "5c0ef8138dc5ae7039fe1503cd2efbb3ab705b00",
-  "shortCommitHash": "5c0ef81",
-  "commitTimestamp": "2026-06-10T03:15:36-03:00",
+  "commitHash": "b4786d6ffc2aa02e8761516b4294929ad328cf51",
+  "shortCommitHash": "b4786d6",
+  "commitTimestamp": "2026-06-10T03:47:23-03:00",
   "branch": "main",
   "deployId": "local",
-  "buildTimestamp": "2026-06-10T06:34:55.275Z",
+  "buildTimestamp": "2026-06-10T06:47:23.316Z",
   "githubRepo": "ae95caba/dulce-tentacion-admin"
 } as const;
